efc723531fd5dcde295d919dc08c67455efd1a8b
PLASTICPEO <giodavlasheridze88@gmail.com>
2023-02-05 19:35:02 +0400

    Phonebook Part 6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  const [note, setNotes] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setNotes(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      {note.map((item) => (
+        <p key={item.id}>
+          {item.name} {item.number}
+        </p>
+      ))}
+    </div>
+  );
 }
 
 export default App;
