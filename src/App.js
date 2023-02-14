e5118bf78290527886fa87a94b34ba7d74141ebf
PLASTICPEO <giodavlasheridze88@gmail.com>
2023-02-14 20:56:28 +0400

    countries info

--- src/App.js
+++ src/App.js
@@ -2,23 +2,71 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-function App() {
-  const [note, setNotes] = useState([]);
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [seachValue, setSearchValue] = useState("");
+  const [fleg, setFleg] = useState("");
+
   useEffect(() => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setNotes(response.data);
+    axios.get(`https://restcountries.com/v3.1/all`).then((response) => {
+      setCountries(response.data);
     });
   }, []);
 
+  const allCountries = countries.filter((countriesName) => {
+    return countriesName.name.common.toLowerCase().includes(seachValue);
+  });
+
+  useEffect(() => {
+    if (allCountries.length === 1) {
+      console.log(allCountries);
+      setFleg(allCountries[0].flags.png);
+    }
+  }, [allCountries]);
+
   return (
     <div>
-      {note.map((item) => (
-        <p key={item.id}>
-          {item.name} {item.number}
-        </p>
-      ))}
+      <div>
+        Find countries
+        <input type="search" onChange={(e) => setSearchValue(e.target.value)} />
+        <div>
+          {seachValue
+            ? allCountries.map((item, index) => {
+                if (allCountries.length === 1) {
+                  return (
+                    <div key={index}>
+                      <h2>{item.name.common}</h2>
+                      <p style={{ fontWeight: "bold" }}>
+                        Capital: {item.capital[0]}
+                      </p>
+                      <p style={{ fontWeight: "bold" }}>area: {item.area}</p>
+                      <div style={{ fontWeight: "bold" }}>
+                        Language:
+                        <ul>
+                          {Object.values(item.languages).map(
+                            (language, index) => {
+                              return <li key={index}>{language}</li>;
+                            }
+                          )}
+                        </ul>
+                        <img src={fleg} alt="icons" />
+                      </div>
+                    </div>
+                  );
+                } else {
+                  return (
+                    <p key={index}>
+                      {item.name.common}
+                      <button>Show</button>
+                    </p>
+                  );
+                }
+              })
+            : null}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
